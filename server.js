c4319ad15a75af6b2e1d04550d3ad900419f5fc9
Gautam Arora <gautamarora6248@gmail.com>
2020-06-17 13:46:22 +0530

    Major Changes

--- server.js
+++ server.js
@@ -13,30 +13,6 @@ var port = process.env.PORT || 8000;
 app.get("/", function(req, res) {
     res.sendFile("index.html", { root: __dirname });
 });
-// ``let transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.PASSWORD,
-//     },
-// });
-
-// app.post("/sendmail", function(req, res) {
-//     let mailOptions = {
-//         from: req.body.email,
-//         to: "gautamarora6248@gmail.com",
-//         text: req.body.message,
-//     };
-
-//     transporter.sendMail(mailOptions, function(err, data) {
-//         if (err) {
-//             console.log("Error Occurs", err);
-//         } else {
-//             console.log("Message Sent");
-//         }
-//     });
-//     res.redirect("/");
-// });``
 
 app.listen(port, function(req, res) {
     console.log("Server has been started in port 8000");
